d9848b3180fef88019e596e3a760a87bd8989dee
ppcano <ppcanodehuelva@gmail.com>
2011-10-07 18:04:00 +0200

    modified

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 
-//var server = require('express').HTTPServer.prototype
-var  express = require('express');
-  //, cfg = require('cfg')
-  //, fs = require('fs')
-  //, path = require('path');
+var  express = require('express')
+  , cfg = require('cfg')
+  , fs = require('fs')
+  , path = require('path');
 
 exports.version = '0.0.1';
 
 // create cfg items and set app path variables 
 // when app_path is undefined, it will look up it.
-/* 
 exports.createConfig = function(app_path){
 
   var config = cfg.createConfig(); 
@@ -42,40 +40,17 @@ exports._lookUpRootAppDir = function(dir){
 
   return dir;
 };
-*/
-/*
-var MyResource = module.exports = function MyResource(name, actions, app) {
+
+
+
+
+
+
+express.HTTPServer.prototype.useCfg =
+express.HTTPSServer.prototype.useCfg = function(app_path){
+
+  this.cfg = exports.createConfig(app_path);
 
 };
-*/
 
 
-
-
-//module.exports = express;
-
-//connect.prototype.cfg = function(){};
-
-console.log('-----------------------------'+ express.version);
-
-//module.exports = require('./lib/index');
-
-//module.exports = express.HTTPServer;
-
-//module.exports = exports = server;
-
-//module.exports.NotFoundRouteError = require('./lib/errors').NotFoundRouteError;
-//module.exports.NotFoundModelError = require('./lib/errors').NotFoundModelError;
-
-
-express.HTTPServer.prototype.usecfg =
-express.HTTPSServer.prototype.usecfg = function(name, actions, opts){
-  return null;
-};
-
-
-console.log('---------------------------------useCFg');
-//console.log( connect.prototype.cfg);
-console.log( express.HTTPServer.prototype.usecfg);
-//console.log( express.HTTPSServer.prototype);
-console.log('---------------------------------useCFg');

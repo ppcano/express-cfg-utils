22e9ad4f1186f86efabbad5f982e30e522ddfdf6
ppcano <ppcanodehuelva@gmail.com>
2011-10-08 14:02:50 +0200

    fixed: npm link problem

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
+module.exports.version = '0.0.1';
 
-module.exports = require('./lib/index');
 module.exports.NotFoundRouteError = require('./lib/errors').NotFoundRouteError;
 module.exports.NotFoundModelError = require('./lib/errors').NotFoundModelError;
+module.exports.Loader = require('./lib/loader');

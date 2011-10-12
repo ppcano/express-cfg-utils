18de6f280550d85b1e7e7690237c983696f523c9
Jose Cano <ppcanodehuelva@gmail.com>
2011-10-12 14:03:53 +0200

    adding ForbiddenError

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 module.exports.version = '0.0.1';
 
+module.exports.ForbiddenError = require('./lib/errors').ForbiddenError;
 module.exports.NotFoundRouteError = require('./lib/errors').NotFoundRouteError;
 module.exports.NotFoundModelError = require('./lib/errors').NotFoundModelError;
 module.exports.Loader = require('./lib/loader');
